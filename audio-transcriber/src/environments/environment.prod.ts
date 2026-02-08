bfa77f69b7a8c2d51f14798be8a9079e8e536b9c
suda27 <susasan27@gmail.com>
2026-02-08 14:03:49 +0530

    Added the env files

---
+++ audio-transcriber/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+// Production environment
+export const environment = {
+  production: true,
+  openaiApiKey: '{{OPENAI_API_KEY}}' // Will be replaced at build time
+};
+
---
+++ audio-transcriber/src/environments/environment.ts
@@ -0,0 +1,6 @@
+// Development environment
+export const environment = {
+  production: false,
+  openaiApiKey: '{{OPENAI_API_KEY}}' // Will be replaced at build time
+};
+
